7eae6079698e4468ef554256ff39fadd39803a87
YemiS <yemi.jaiyeola@btinternet.com>
2020-03-22 19:07:14 +0000

    add config

--- src/functions/extract/index.js
+++ src/functions/extract/index.js
@@ -1,36 +1,21 @@
 const chunkSize = 500;
-exports.handler = (event, context, callback) => {
+exports.handler = (event, context) => {
     var result = event.hasOwnProperty('results') ? event.results : { procesedRows: 0, importedRows:0, errors: [] };
-  ...
+  console.log(result);
    // now we take care of chunks and update output param if Eof is reached
 
    let index =0,
    processed = 0,
    end = true;
+   
 
-   let pipeline = s3.getObject({
+  /* let pipeline = s3.getObject({
        Bucket:'MyBucket',
        Key: 'Mykey'
-   }).createReadStream()
-   .pipe(es.split(/\r|\r?\n/))
-   .pipe(es.mapSync((line) => {
-       index++;
-       if ((resulst.procesedRows + 1) < index) {//forward processed records
-        if (processed < chunkSize) {
-            processed++; result.procesedRows++;
-            pipeline.pause();
-            process(line);
-        } else {
-            end = false; // we already processed our chunk but no EOF
-            pipeline.end();
-        }
+   });*/
 
+  
+    return  event['result']['finished'] = end;
 
-       }
-
-   })).on('end', () => {
-       result.finished = end; //output if EOF reached
-   });
-   ...
 
 }
